93d79399885e0bdd3a256a8e215704e4cec5bf2a
Slylnss <s.alonsious@web-siouss.be>
2017-11-08 10:55:30 +0100

    finition select.sql creation insert.sql

--- SELECT.sql
+++ SELECT.sql
@@ -71,32 +71,78 @@ SELECT firstname, lastname FROM octocats where promo = 'promo1-central';
 
 // Exercice 6
 
-SELECT firstname, lastname, birthdate FROM octocats where promo = 'promo1-anderlecht'
+SELECT firstname, lastname, birthdate FROM octocats where promo = 'promo1-anderlecht';
 
 
 ///// DRILL 3 //////
 
 // Exercice 1
 
-SELECT firstname, lastname FROM octocats ORDER BY firstname ASC
+SELECT firstname, lastname FROM octocats ORDER BY firstname ASC;
 
 // Exercice 2
 
-SELECT firstname, lastname FROM octocats ORDER BY lastname DESC
+SELECT firstname, lastname FROM octocats ORDER BY lastname DESC;
 
 // Exercice 3
 
-SELECT firstname, lastname, age FROM octocats ORDER BY age ASC
+SELECT firstname, lastname, age FROM octocats ORDER BY age ASC;
 
 // Exercice 4
 
-SELECT firstname, lastname, age FROM octocats ORDER BY age DESC
+SELECT firstname, lastname, age FROM octocats ORDER BY age DESC;
 
 // Exercice 5
 
-SELECT firstname, lastname, age FROM octocats WHERE promo = 'promo1-central' ORDER BY age ASC
+SELECT firstname, lastname, age FROM octocats WHERE promo = 'promo1-central' ORDER BY age ASC;
 
 //Exercice 6
+
+SELECT firstname, lastname, age FROM octocats WHERE promo = 'promo1-central'AND age>23 AND age<28 ORDER BY age ASC;
+
+// Exercice 7
+
+SELECT firstname, lastname, age, gender FROM octocats WHERE promo = 'promo1-central' AND age>25 AND age<35 ORDER BY age ASC;
+
+// Exercice 8
+
+SELECT firstname, lastname, age, gender FROM octocats WHERE promo = 'promo1-central' AND age>25 AND age<35 AND gender='M' ORDER BY age ASC;
+
+// Exercice 9
+
+SELECT firstname, lastname, age FROM octocats WHERE promo='promo1-central' ORDER BY age DESC LIMIT 0,1;
+
+
+///// DRILL 3 /////
+
+// Exercice 1
+
+SELECT count(*) FROM octocats WHERE gender='M';
+
+// Exercice 2
+
+SELECT count(*) FROM octocats WHERE gender='F' AND promo='promo1-central';
+
+// Exercice 3
+
+SELECT count(*) FROM octocats WHERE gender='M' AND promo='promo1-central';
+
+// Exercice 4
+
+SELECT count(*) FROM octocats WHERE firstname = 'Nadia';
+
+// Exercice 5
+
+SELECT firstname, YEAR(birthdate) from octocats;
+
+// Exercice 6
+
+SELECT concat('Demain le maxima observé sera de ', haut, 'degrés') FROM Météo;
+
+// Exercice 7
+
+SELECT concat('Demain le minimale observé sera de ', bas, ' degrés') FROM Météo;
+
 
 
 
